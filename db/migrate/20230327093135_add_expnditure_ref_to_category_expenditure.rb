172870d639a47808d5ce155feedaeb6c8df62b0e
anyepeter <petzyrockchendi@gmail.com>
2023-03-27 11:14:55 +0100

    add category and expenditure

---
+++ db/migrate/20230327093135_add_expnditure_ref_to_category_expenditure.rb
@@ -0,0 +1,5 @@
+class AddExpnditureRefToCategoryExpenditure < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :category_expenditures, :expenditure, null: false, foreign_key: true
+  end
+end
