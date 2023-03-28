024c1d47cc0723b86092710b1aabe79638326cfd
anyepeter <petzyrockchendi@gmail.com>
2023-03-28 11:07:16 +0100

    connect expenditure and category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,22 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
-
+  load_and_authorize_resource :through => :current_user
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
-  # GET /categories/1 or /categories/1.json
-  def show; end
-
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit; end
-
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.build(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
