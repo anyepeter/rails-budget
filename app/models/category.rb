6219e05189242f4ba9db0f3d29774f2731496956
anyepeter <petzyrockchendi@gmail.com>
2023-03-28 11:05:20 +0100

    create a method to add category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,17 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :expenditures, through: :category_expenditures
   has_many :category_expenditures
+  has_many :expenditures, through: :category_expenditures
+  
 
   validates :name, presence: true
   validates :icon, presence: true
+
+  def sumOfexpenditures(x)
+     sum = 0 
+     x.each do |expenditure| 
+     sum += expenditure.amount 
+    end 
+   sum
+  end
 end
